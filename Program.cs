1fa72e3b567908066adee3362320afadee04d7f3
JosineudoA <96135176+JosineudoA@users.noreply.github.com>
2022-05-14 21:00:44 -0300

    Inserção do Lab no SGBD

--- Program.cs
+++ Program.cs
@@ -19,6 +19,19 @@ command.CommandText = @"
 ";
 command.ExecuteNonQuery();
 connection.Close();
+
+connection.Open();
+
+command.CommandText = @"
+    CREATE TABLE IF NOT EXISTS Labs(
+        id int not null primary key,
+        number varchar(100) not null,
+        name varchar(100) not null,
+        block varchar(100) not null
+    );
+";
+command.ExecuteNonQuery();
+connection.Close();
 //Routing
 var modelName = args[0];
 var modelAction = args[1];
@@ -60,6 +73,50 @@ if(modelName == "Computer")
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
         command.Parameters.AddWithValue("$processor", processor);
+        
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
+
+if(modelName == "Lab")
+{
+    if(modelAction == "List")
+    {
+        Console.WriteLine("Lab List");
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+        
+        command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Labs;";
+        
+        var reader = command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            Console.WriteLine("{0},{1},{2},{3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+        }
+        
+        reader.Close();
+        connection.Close();
+    }
+
+    if(modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        var name = args[4];
+        var block = args[5];
+        
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+        
+        command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Labs VALUES ($id, $number, $name, $block);";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
         
         command.ExecuteNonQuery();
         connection.Close();
