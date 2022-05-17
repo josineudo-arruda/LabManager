ad409f0352c39a38e51578bfd70cb8db0273cc18
josineudo-arruda <josineudo.arruda@gmail.com>
2022-05-17 18:03:23 -0300

    refactor: add computer class in Repositories

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
-
 using LabManager.Database;
+using LabManager.Repositories;
 
 //Routing
 var modelName = args[0];
@@ -8,26 +8,17 @@ var modelAction = args[1];
 
 new DatabaseSetup();
 
+var computerRepository = new ComputerRepository();
+
 if(modelName == "Computer")
 {
     if(modelAction == "List")
     {
         Console.WriteLine("Computer List");
-        var connection = new SqliteConnection("Data Source=database.db");
-        connection.Open();
-        
-        var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Computers;";
-        
-        var reader = command.ExecuteReader();
-
-        while(reader.Read())
+        foreach (var computer in computerRepository.GetAll())
         {
-            Console.WriteLine("{0},{1},{2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+            Console.WriteLine("{0},{1},{2}", computer.Id, computer.Ram, computer.Processor);
         }
-        
-        reader.Close();
-        connection.Close();
     }
 
     if(modelAction == "New")
