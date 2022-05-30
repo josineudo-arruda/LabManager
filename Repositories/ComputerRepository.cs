5a4eb53f2dbe55e99fcdbc0c52bc21c8f478c4fc
JosineudoA <96135176+JosineudoA@users.noreply.github.com>
2022-05-29 21:43:34 -0300

    enhacement: add computer repository delete method

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -83,4 +83,17 @@ class ComputerRepository
 
         return computer;
     }
+
+    public void Delete(int id)
+    {
+        var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "DELETE FROM Computers WHERE id = $id;";
+        command.Parameters.AddWithValue("$id", id);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
 }
